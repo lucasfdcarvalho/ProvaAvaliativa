b95dc95b9f33cd84af1df72420226e0bd43a6a13
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-03 23:22:57 -0300

    Adicionando a classe Produto que contém ID, Nome, Preço e Categoria como atributos, além disso esses valores vão ser passados no construtor da classe e dentro dele, serão feitas as verificações necessárias a respeitos dos valores. Caso todos os valores estejam corretos são atribuídos aos seus respectivos atributos, caso ao contrário, o programa irá parar e direcionar para o local do erro

---
+++ ProvaAvaliativa/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class Produto
+    {
+        public int ID { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public double Preco { get; private set; }
+
+        public string Categoria { get; private set; }
+
+        public Produto(int iD, string nome, double preco, string categoria)
+        {
+            if(iD <= 0)
+            {
+                throw new Exception("ID do produto inválido");
+            }
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new Exception("Nome do produto inválido");
+            }
+            if(preco <= 0)
+            {
+                throw new Exception("Preço do produto inválido");
+            }
+            if (string.IsNullOrWhiteSpace(categoria))
+            {
+                throw new Exception("Categoria do produto inválida");
+            }
+
+            this.ID = iD;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Categoria = categoria;
+        }
+    }
+}
