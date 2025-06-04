c1d4f09e055ea819740d6d732bd0d83798bc6a07
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 13:55:04 -0300

    Foram criadas a classe ItemPedido e a interface ISubTotal. A classe ItemPedido tem como atributos o produto que é do tipo Produto(associação), Quantidade do tipo int(representa a quantidade do item/produto) e o SubTotal do tipo double que calcula dinamicamente o preço do produto * quantidade gerando o subtotal. No método construtor foi feita a validação da quantidade por meio de um método separado e, caso não houvesse nenhum erro, seriam atribuídos os valores passados como parâmetro aos atributos produto e Quantidade. Além disso a classe implementa a interface ISubTotal que contém o método CalcularSubTotal() que retorna um valor do tipo double, ou seja, retorna o atributo SubTotal. Assim, nessa etapa, podemos perceber a aplicação do Princípio da Responsabilidade Única na qual a classe ItemPedido tem atributos e métodos únicos deles(estão de acordo com o seu propósito/abstração) e também do Princípio da Segregação de Interfaces, já que a classe ItemPedido implementa a interface ISubTotal e seu método CalcularSubTotal.

---
+++ ProvaAvaliativa/ItemPedido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class ItemPedido : ISubTotal
+    {
+        public Produto produto { get; private set; }
+
+        public int Quantidade { get; private set; }
+
+        public double SubTotal => produto.Preco * Quantidade;
+
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            ValidarQuantidade(quantidade);
+
+            this.produto = produto;
+            this.Quantidade = quantidade;
+        }
+
+        private void ValidarQuantidade(int quantidade)
+        {
+            if(quantidade <= 0)
+            {
+                throw new ArgumentException("Quantidade inválida");
+            }
+        }
+
+        public double CalcularSubTotal()
+        {
+            return SubTotal;
+        }
+    }
+}
