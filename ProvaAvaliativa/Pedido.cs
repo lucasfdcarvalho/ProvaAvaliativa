4210bb5e474ff0eeff60726ac492247b755eef8e
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 19:06:18 -0300

    Foi criado a classe Pedido que vai possuir os seguintes atributos: ID(int), Cliente(Cliente), Lista de Itens(ItemPedido), Data(DateTime), ValorTotal(double) e descontoStrategy(IDescontoStrategy). No método construtor foram passados como parâmetro o id, cliente e a estratégia de desconto(por categoria ou por quantidade), assim, é feita uma validação para verificar se o id está correto, caso esteja, esses valores são atribuídos às seus respectivos atributos(o atributo descontoStrategy só permite uma única atribuição), é inicializado a lista de itens e, também, atribui-se a data de hoje no atributo Data.

---
+++ ProvaAvaliativa/Pedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class Pedido
+    {
+        public int ID { get; private set; }
+
+        public Cliente Cliente { get; private set; }
+
+        public List<ItemPedido> Itens { get; private set; }
+
+        public DateTime Data { get; private set; }
+
+        public double ValorTotal { get; private set; }
+
+        private readonly IDescontoStrategy descontoStrategy;
+
+        public Pedido(int id, Cliente cliente, IDescontoStrategy descontoStrategy)
+        {
+            ValidarId(id);
+
+            this.ID = id;
+            this.Cliente = cliente;
+            this.descontoStrategy = descontoStrategy;
+            Itens = new List<ItemPedido>();
+            Data = DateTime.Now;
+        }
+
+        private void ValidarId(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentException("ID inválido");
+            }
+        }
+    }
+}
