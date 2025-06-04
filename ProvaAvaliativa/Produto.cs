ef0f09ec9a8f4d1653441de2860811e2dd78bd12
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 00:05:42 -0300

    A classe Produto foi alterada devido ao fato de a classe construtora estar muito massante de verificações e de IFs. Assim cada validação foi isolada separadamente, atarvés de métodos privados, permitindo um menor acoplamento de verificações na classe construtora

--- ProvaAvaliativa/Produto.cs
+++ ProvaAvaliativa/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,27 +19,47 @@ namespace ProvaAvaliativa
 
         public Produto(int iD, string nome, double preco, string categoria)
         {
-            if(iD <= 0)
-            {
-                throw new Exception("ID do produto inválido");
-            }
-            if (string.IsNullOrWhiteSpace(nome))
-            {
-                throw new Exception("Nome do produto inválido");
-            }
-            if(preco <= 0)
-            {
-                throw new Exception("Preço do produto inválido");
-            }
-            if (string.IsNullOrWhiteSpace(categoria))
-            {
-                throw new Exception("Categoria do produto inválida");
-            }
+            ValidarId(iD);
+            ValidarNome(nome);
+            ValidarPreco(preco);
+            ValidarCategoria(categoria);
 
             this.ID = iD;
             this.Nome = nome;
             this.Preco = preco;
             this.Categoria = categoria;
+        }
+
+        private void ValidarId(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("ID do produto inválido");
+            }
+        }
+
+        private void ValidarNome(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new Exception("Nome do produto inválido");
+            }
+        }
+
+        private void ValidarPreco(double preco)
+        {
+            if (preco <= 0)
+            {
+                throw new Exception("Preço do produto inválido");
+            }
+        }
+
+        private void ValidarCategoria(string categoria)
+        {
+            if (string.IsNullOrWhiteSpace(categoria))
+            {
+                throw new Exception("Categoria do produto inválida");
+            }
         }
     }
 }
