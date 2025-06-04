09c3dbd6051da5d902cac28aa78c11c01f9402c3
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 15:17:49 -0300

    Foram criados a interface IDescontoStrategy(define um contrato para o cálculo do desconto) e a classe DescontoPorQuantidade. Para a formulação do desconto foi usado o padrão de projeto Strategy, permitindo que diferentes regras de descontos sejam trocados de forma flexível. A interface IDescontoStrategy define o método CalcularDesconto(Pedido pedido) que vai ser responsável por calcular os desconto baseado no pedido. A classe DescontoPorQuantidade implementa IDescontoStrategy e possui dois atributos: QuantidadeMinima e PercentualDesconto. O principal objetivo dessa classe é calcular o desconto baseado em uma quantidade mínima de itens. No método construtor da classe são definidos e verificados os valores que serão atribuídos aos atributos. Os atributos forma definidos como readonly pois permite apenas uma única atribuição que ocorre no método construtor. Além disso a classe ainda apresenta outros métodos para validações dos valores quantidadeMinima e percentualDesconto. Princípios aplicados: Princípio da Responsabilidade Única(DescontoPorQuantidade tem uma única responsabilidade, que é calcular o desconto com base na quantidade mínima , sem misturar outras lógicas), Princípio da Segregação de Interfaces(a interface IDescontoStrategy define apenas o método CalcularDesconto, evitando que implementações fiquem obrigadas a métodos desnecessários), Princípio Aberto Fechado(novas estratégias de descontos podem ser criadas e adicionadas, implementando IDescontoStrategy, sem alterar o código existente de DescontoPorQuantidade ou da interface) e Princípio da Substituição de Liskov(princípio que ocorre de forma indireta, já que qualquer implementação de IDescontoStrategy pode ser usada no lugar de outra sem quebrar o comportamento esperado)

---
+++ ProvaAvaliativa/DescontoPorQuantidade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class DescontoPorQuantidade : IDescontoStrategy
+    {
+        private readonly int QuantidadeMinima;
+
+        private readonly double PercentualDesconto;
+
+        public DescontoPorQuantidade(int quantidadeMinima = 5, double percentualDesconto = 0.10)
+        {
+            ValidarQuantidadeMinima(quantidadeMinima);
+            ValidarPercentual(percentualDesconto);
+
+            this.QuantidadeMinima = quantidadeMinima;
+            this.PercentualDesconto = percentualDesconto;
+        }
+
+        private void ValidarQuantidadeMinima(int quantidadeMinima)
+        {
+            if(quantidadeMinima <= 0)
+            {
+                throw new ArgumentException("Quantidade Mínima inválida");
+            }
+        }
+
+        private void ValidarPercentual(double percentualDesconto)
+        {
+            if(percentualDesconto < 0 || percentualDesconto > 1)
+            {
+                throw new ArgumentException("O percentual de desconto deve estar entre 0 e 1");
+            }
+        }
+
+        public double CalcularDesconto(Pedido pedido)
+        {
+            
+        }
+    }
+}
---
+++ ProvaAvaliativa/IDescontoStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public interface IDescontoStrategy
+    {
+        double CalcularDesconto(Pedido pedido);
+    }
+}
