ef86eb8f0bb4ba2304ac3d6187ded573f786c4da
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 15:40:49 -0300

    Foi adicionado a classe DescontoPorCategoria, que implementa a interface IDescontoStrategy. Essa classe, assim como a classe DescontoPorQuantidade, também implementa o padrão de projeto Strategy por meio da interface IDescontoStrategy. Diferentemente da classe DescontoPorQuantidade, essa classe implementa outra regra de desconta baseado na categoria do pedido, assim ela possui dois atributos: categoriaAlvo e percentualDesconto, ambos os atributos são readonly, isto é, podem ser atribuídos somente uma única vez(que ocorre no método construtor). Além disso, os valores foram passados como parâmetros no método construtor e feitas as verificações em métodos separados. Dessa forma caso não houvesse nenhum erro, os valores seriam atribuídos aos atributos. A classe implementa o método CalcularDesconto(Pedido pedido) que será responsável por calcular o desconto. Os princípios utilizados foram os mesmos da classe DescontoPorQuantidade

---
+++ ProvaAvaliativa/DescontoPorCategoria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class DescontoPorCategoria : IDescontoStrategy
+    {
+        private readonly string categoriaAlvo;
+
+        private readonly double percentualDesconto;
+        
+        public DescontoPorCategoria(string categoriaAlvo, double percentualDesconto = 0.10)
+        {
+            ValidarCategoriaAlvo(categoriaAlvo);
+            ValidarPercentual(percentualDesconto);
+
+            this.categoriaAlvo = categoriaAlvo;
+            this.percentualDesconto = percentualDesconto;
+        }
+
+        private void ValidarCategoriaAlvo(string categoriaAlvo)
+        {
+            if (string.IsNullOrWhiteSpace(categoriaAlvo))
+            {
+                throw new ArgumentException("Categoria inválida");
+            }
+        }
+
+        private void ValidarPercentual(double percentualDesconto)
+        {
+            if (percentualDesconto < 0 || percentualDesconto > 1)
+            {
+                throw new ArgumentException("O percentual de desconto deve estar entre 0 e 1");
+            }
+        }
+
+        public double CalcularDesconto(Pedido pedido)
+        {
+
+        }
+    }
+}
