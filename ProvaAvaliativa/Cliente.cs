8e4685bd0c3f35f5ede6ebbfb552a864ff25b7d0
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-04 00:01:13 -0300

    Adicionando a classe Cliente com os seguintes atributos: ID, Nome, Email e CPF. Na classe construtor foi feita as validações dos respectivos valores passados como parâmetros, feitas através de métodos privados e independentes, evitando que a classe construtora ficasse muito massiva. Caso houvesse algum erro em um dos valores dos atributos, o programa se encerrará e direcionará ao local do erro. Caso não tenha nenhum erro, os valores são atribuídos aos atributos da classe

---
+++ ProvaAvaliativa/Cliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class Cliente
+    {
+        public int ID { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string CPF { get; private set; }
+
+        public Cliente(int iD, string nome, string email, string cPF)
+        {
+            ValidarId(iD);
+            ValidarNome(nome);
+            ValidarEmail(email);
+            ValidarCpf(cPF);
+
+            this.ID = iD;
+            this.Nome = nome;
+            this.Email = email;
+            this.CPF = cPF;
+        }
+
+        private void ValidarId(int id)
+        {
+            if (id <= 0)
+            {
+                throw new Exception("ID do cliente inválido");
+            }
+        }
+
+        private void ValidarNome(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new Exception("Nome do cliente inválido");
+            }
+        }
+
+        private void ValidarEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("E-mail do cliente inválido");
+            }
+        }
+
+        private void ValidarCpf(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf) || cpf.Length != 11 || !cpf.All(char.IsDigit))
+            {
+                throw new Exception("CPF inválido");
+            }
+        }
+    }
+}
