cf4f989fb7afdac25435cd649c1dfba63f34ba89
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-05 17:14:16 -0300

    Foram adicionados a interface IRelatorioPedido e a classe RelatorioPedido. A interface IRelatorioPedido contém os métodos AdicionarPedido e GerarRelatorio e essa interface foi implementada na classe RelatorioPedido que tem como objetivo principal exibir um relatório sobre cada pedido, dessa forma, a classe possui uma lista de pedidos e os os dois métodos que foram estabelecidas na interface. Os princípios aplicados foram: Princípio da Responsabilidade Única(separando as responsabilidades de armazenar pedidos e exibir relatório), Princípio da Inversão de Depêndencia(pois trabalha com a abstração IRelatorioPedido em vez de uma classe concreta) e Princípio Aberto Fechado(novas formas de relatório podem ser criadas apenas implementando IRelatorioPedido, sem modificar o código existente)

---
+++ ProvaAvaliativa/RelatorioPedido.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class RelatorioPedido : IRelatorioPedido
+    {
+        private List<Pedido> pedidos = new List<Pedido>();
+
+        public void AdicionarPedido(Pedido pedido)
+        {
+            pedidos.Add(pedido);
+            Console.WriteLine("Pedido adicionado ao relatório com sucesso!");
+        }
+
+        public void GerarRelatorio()
+        {
+            if(pedidos.Count == 0)
+            {
+                Console.WriteLine("Nenhum pedido cadastrado!");
+                return;
+            }
+
+            foreach(Pedido pedido in pedidos)
+            {
+                Console.WriteLine($"Pedido: #{pedido.ID}\nCliente: {pedido.Cliente.Nome} (CPF: {pedido.Cliente.CPF})\nData do Pedido: {pedido.Data:G}\n");
+
+                Console.WriteLine("Itens:");
+                foreach(ItemPedido item in pedido.Itens)
+                {
+                    Console.WriteLine($" - Produto: {item.produto.Nome}");
+                    Console.WriteLine($" - Categoria: {item.produto.Categoria}");
+                    Console.WriteLine($" - Quantidade: {item.Quantidade}");
+                    Console.WriteLine($" - SubTotal: {item.CalcularSubTotal():F2}\n");
+                }
+
+                Console.WriteLine($"Valor Total com desconto: R$ {pedido.ValorTotal:F2}");
+                Console.WriteLine("----------------------------------------------------\n");
+            }
+        }
+    }
+}
