6e8c424b9bc6613e8881bcdc760320808c19b9a0
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-05 14:20:44 -0300

    Foi adcionado a interface IPedidoFactory com o método CriarPedido(int id, Cliente cliente, IDescontoStrategy descontoStrategy) que abstrai a criação de objetos Pedido seguindo o padrão Factory Method. Ela tem o objetivo de isolar e encapsular a lógica de construção do Pedido, permitindo diferentes implementações dessa fábrica sem alterar o código cliente. Os princípios aplicados são: Princípio da Responsabilidade Única(a factory cuida somente de instanciar um Pedido) e Princípio Aberto Fechado(novas maneiras de criar pedidos podem ser adicionadas implementando IPedidoFactory, sem modificar o código existente)

---
+++ ProvaAvaliativa/IPedidoFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public interface IPedidoFactory
+    {
+        Pedido CriarPedido(int id, Cliente cliente, IDescontoStrategy descontoStrategy);
+    }
+}
