4058574ae44d6981f3c19e01f440298eaa934285
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-05 14:33:45 -0300

    Foi adicionada a classe PedidoFactory que implementa a interface IPedidoFactory e a implementação do método CriarPedido, o qual recebe como parêmetros o id, cliente e a estratégia de desconto. Os princípios aplicados são: Princípio da Responsabilidade Única(encapsula apenas a lógica de criação de um pedido sem misturar outras tarefas), Princípio da Segregação de Interface(a classe implementa o método estabelcido na interface IPedidoFactory, sem ser obrigada a implementar funcionalidades indesejadas), Princípio da Inversão de Dependência(a classe depende da interface IPedidoFactory e não de uma implementação) e Princípio Aberto Fechado(novas fábricas podem ser criadas estendendo IPedidoFactory sem modificar o código existente)

---
+++ ProvaAvaliativa/PedidoFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class PedidoFactory : IPedidoFactory
+    {
+        public Pedido CriarPedido(int id, Cliente cliente, IDescontoStrategy descontoStrategy)
+        {
+            return new Pedido(id, cliente, descontoStrategy);
+        }
+    }
+}
