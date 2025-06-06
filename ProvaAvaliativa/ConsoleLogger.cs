ce93dbbf1307b8526eab14f39ecd0b0ce25e3314
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-06 15:25:50 -0300

    Foi adicionado a interface ILogger e a classe ConsoleLogger. ILogger estabelece um contrato para os serviços de log que irão exibir uma mensagem. A classe ConsoleLogger implementa essa interface e registra a mensagem juntamente da data e hora no console. Essa interface vai ser usada para injetar essa classe em outras classes(PedidoFactory, RelatorioPedido e Pedido)

---
+++ ProvaAvaliativa/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaAvaliativa
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string mensagem)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now:G} - {mensagem}");
+        }
+    }
+}
