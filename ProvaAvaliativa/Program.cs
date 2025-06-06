0ba9236c6d489d2305b8a3af7952413e36f1e289
Lucas Freitas de Carvalho <lucasfreitasdecarvalho15@gmail.com>
2025-06-06 16:12:02 -0300

    No início do arquivo foi feita referências aos dois pacotes instalados anteriormente(Microsoft.Extensions.DependencyInjection e Microsoft.Extensions.Hosting) que são responsáveis por permitir a injeção de dependência e o gerenciamento do container de injeção. Logo abaixo foi criado o container de injeção que vai gerenciar os serviços(vale destacar que o container de injeção é responsável por criar instâncias, cuidar dos construtores e gerenciar o ciclo de vida dos serviços). Dentro do container foi definido três serviços: o logger(ILogger), a fábrica de pedidos(IPedidoFactory) e o gerador de relatórios(IRelatorioPedido). O serviço do logger foi definido com o ciclo de vida Singleton que permite apenas uma única instância de ConsoleLogger. Foi escolhido esse tipo de ciclo pois o logger é utilizado em várias partes do programa, ou seja, escolhi esse ciclo pois garantimos um menor consumo de memória. Assim toda vez que a interface ILogger é passado como parâmetro no método construtor de uma classe, o serviço retorna a classe ConsoleLogger para essa classe que fez a requisição. O segundo serviço trata-se da fábrica de pedidos que também foi escolhido com o ciclo de vida Singleton, já que apenas uma instância de PedidoFactory pode ser reaproveitada no código. Dessa forma, toda vez que uma classe chamar a interface IPedidoFactory no seu método construtor, essa classe(PedidoFactory) é retornada. O terceiro serviço trata-se do gerador de relatórios que também foi escolhido com o ciclo de vida Singleton, pois ele também pode ser reaproveitado em várias partes do código sem a necessidade de criar outras instâncias. Com isso, toda a ves que uma classe chamar essa interface IRelatórioPedido no seu método construtor, a classe RelatorioPedido é retornada. Logo abaixo iniciamos os serviços estabelecidos no container que serão usados no restante do código

--- ProvaAvaliativa/Program.cs
+++ ProvaAvaliativa/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 using ProvaAvaliativa;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
+// Configurando e construindo o container de injeção com os serviços
+IHost host = Host.CreateDefaultBuilder(args).ConfigureServices((context, service) =>
+{
+    // Registra um logger que será compartilhado como singleton(única)
+    service.AddSingleton<ILogger, ConsoleLogger>();
+    // Registra a fábrica de pedidos que injeta o ILogger no Pedido
+    service.AddSingleton<IPedidoFactory, PedidoFactory>();
+    // Registra o gerador de relatórios que injeta o ILogger
+    service.AddSingleton<IRelatorioPedido, RelatorioPedido>();
+}).Build();
 
-RelatorioPedido relatorio = new RelatorioPedido();
+// Iniciando os serviços resolvidos pelo container
+ILogger logger = host.Services.GetRequiredService<ILogger>();
+IPedidoFactory pedidoFactory = host.Services.GetRequiredService<IPedidoFactory>();
+IRelatorioPedido relatorio = host.Services.GetRequiredService<IRelatorioPedido>();
 
-// Primeiro pedido: Lucas comprando 3 bicicletas com desconto por quantidade
+// Criando o primeiro cliente e produto
 Cliente cliente1 = new Cliente(25, "Lucas", "example@gmail.com", "22424432412");
 Produto produto1 = new Produto(21, "Bicicleta", 500.5, "Veículo");
 IDescontoStrategy descontoQtd = new DescontoPorQuantidade(2, 0.30);
-IPedidoFactory pedidoFactory1 = new PedidoFactory();
-Pedido pedido1 = pedidoFactory1.CriarPedido(10, cliente1, descontoQtd);
+
+Pedido pedido1 = pedidoFactory.CriarPedido(10, cliente1, descontoQtd);
 pedido1.AdicionarItem(produto1, 3);
+
 relatorio.AdicionarPedido(pedido1);
 
-// Segundo pedido: João comprando 1 notebook com desconto por categoria
+// Criando o segundo cliente e produto
 Cliente cliente2 = new Cliente(23, "João", "example2@gmail.com", "12345678901");
 Produto produto2 = new Produto(34, "Notebook", 2000.0, "Eletrônico");
 IDescontoStrategy descontoCat = new DescontoPorCategoria("Eletrônico", 0.4);
-IPedidoFactory pedidoFactory2 = new PedidoFactory();
-Pedido pedido2 = pedidoFactory2.CriarPedido(11, cliente2, descontoCat);
+
+Pedido pedido2 = pedidoFactory.CriarPedido(11, cliente2, descontoCat);
 pedido2.AdicionarItem(produto2, 1);
+
 relatorio.AdicionarPedido(pedido2);
 
-// Exibe relatório de todos os pedidos
+// Gerando e exibindo o relatório final de todos os pedidos adicionados
 relatorio.GerarRelatorio();
